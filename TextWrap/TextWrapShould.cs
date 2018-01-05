cd6a9134bd1be4bff7e81ca8218ed6a5a4ea160a
Jorge <r3b0rn.jhs@gmail.com>
2018-01-05 20:54:25 +0000

    RED not_wrap_if_enough_columns

--- TextWrap/TextWrapShould.cs
+++ TextWrap/TextWrapShould.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace TextWrap
 {
     public class TextWrapShould
     {
+        [Fact]
+        public void not_wrap_if_enough_columns()
+        {
+            Wrap("hello", 7).Should().Be("hello");
+        }
 
         public string Wrap(string text, int columns)
         {
